b15e40b3728239bb53b3997f71d07afc2c5ea80c
rtb <rs.bjerg@gmail.com>
2019-05-22 00:17:07 +0200

    En main fil til at loade alle scripts etc.

---
+++ LAmain.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from sympy import *
+init_printing(use_unicode=True)
+print("sympy importeret\nUnicode sat")
+
+from ekspFunk15_2 import *
+
+
+print("Alle scripts importeret")
