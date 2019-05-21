226262fd44c7129e384b9884ed9a90a4f9fd754d
rtb <rs.bjerg@gmail.com>
2019-05-22 00:13:49 +0200

    Tester om funktionen er en eksponential funktion for matrixen

---
+++ ekspFunk15_2.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+from sympy import *
+
+def ekspFunk15_2(A, F):
+    """
+    Til at tjekke om en funktion
+    er en eksponentialfunktion for
+    en kvadratisk matrix.
+    Følger def. 15.2
+
+    A = den kvadratiske matrix
+    F = funktionen der skal tjekkes
+    t = variablen/parameteren til F
+
+    A skal være en matrix
+    F skal defineres som normalt:
+        def F(t):
+            <kode>...
+    t skal være erklæret som en variabel:
+        t = symbols("t")
+
+    Endelig kaldes metoden som
+    ekspFunk15_2(A, F, t)
+    og ikke
+    ekspFunk15_2(A, F(t), t)
+    eller lignende.
+    """
+
+    # Tjek at matrixen faktisk er kvadratisk
+    dim = A.shape
+    if dim[0] != dim[1]:
+        print("Matrixen er ikke kvadratisk")
+        return False
+
+    # Parameteren til funktionen
+    t = symbols("t")
+
+    # 1.
+    if F(0) == eye(dim[0]):
+        print("1: Holder")
+    else:
+        print("1: Fejler")
+        return False
+
+    # 2.
+    if nsimplify(A*F(t))==diff(F(t),t):
+        print("2: Holder")
+    else:
+        print("2: Fejler")
+        return False
+
+    # 3.
+    if A*F(t)==F(t)*A:
+        print("3: Holder")
+    else:
+        print("3: Fejler")
+        return False
+
+    # Alle holdte
+    return True
