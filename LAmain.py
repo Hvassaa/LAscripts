638285ad77ef4fdc5cb5fc1f3ce623d8dd62a077
rtb <rs.bjerg@gmail.com>
2019-05-22 15:02:14 +0200

    Tilføjede nogle testværdier til LAmain (de skal nok slettes senere)

--- LAmain.py
+++ LAmain.py
@@ -10,3 +10,21 @@ from invertable import *
 from diffLig import *
 
 print("Alle scripts importeret")
+
+
+
+# Nogle test-værdier
+testM = Matrix([
+        [5,2,-1],
+        [2,2,2],
+        [-1,2,5]
+        ])
+
+testBegVær = Matrix([
+        [1],
+        [0],
+        [1]
+        ])
+
+def testF(t):
+    return (S(1)/6)*exp(6*t)*A-(S(1)/6)*A+eye(3)
